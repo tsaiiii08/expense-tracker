d02070d6f5b4652e17e3ab46862b718e864e86c3
tsaiiii08 <lintsaiting0824@gamil.com>
2023-08-06 16:37:02 +0800

    add 'new a expense' function with mongoose

--- routes/modules/expense.js
+++ routes/modules/expense.js
@@ -6,6 +6,11 @@ router.get('/new', (req, res) => {
   res.render('new')
 })
 
+router.post('/new', (req, res) => {
+  const { name, date, category, amount } = req.body
+  Expense.create({ name, date, category, amount })
+    .then(()=> res.redirect('/'))
+})
 
 router.get('/edit', (req, res) => {
   res.render('edit')
