0aa4fdde51930370667eeaf1300f26308415a021
tsaiiii08 <lintsaiting0824@gamil.com>
2023-08-06 18:27:20 +0800

    modify router of home with mongoose

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,22 @@
 const express = require('express')
 const router = express.Router()
+const Expense = require('../../models/expense')
 
 router.get('/', (req, res) => {
- res.render('index')
+  let sum = 0
+  Expense.find()
+  .lean()
+  .then(expenses => {
+    expenses.forEach(expense => {
+      expense.date = expense.date.toJSON().slice(0, 10)
+      sum += expense.amount
+    })
+    return expenses
+  })
+  .then(expenses => {
+    res.render('index', { expenses, sum })
+  })
+  .catch(err => console.log(err))
 })
 
 module.exports = router
