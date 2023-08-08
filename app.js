ffb995031df8393fed43b742d6ef43331132f8de
tsaiiii08 <lintsaiting0824@gamil.com>
2023-08-08 23:01:24 +0800

    add message partials

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const usePassport = require('./config/passport')
 const port = process.env.PORT||3000
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -23,9 +24,12 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: true })) 
 app.use(methodOverride('_method'))
 usePassport(app)
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
+  res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
   next()
 })
 app.use(routes)
