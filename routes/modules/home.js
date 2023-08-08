9e9e58092c9fc56fb6a8efa6a5fdee9f5110bfeb
tsaiiii08 <lintsaiting0824@gamil.com>
2023-08-08 19:22:38 +0800

    add function for view expenses sorted by category

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,8 +9,8 @@ router.get('/', async(req, res) => {
   let rawExpenses = await Expense.find().lean()
   await Promise.all(
     rawExpenses.map(async rawExpense => {
-    const catergory = await Category.findOne({ id: rawExpense.categoryId }).lean()
-    rawExpense.icon = catergory.icon
+    const categoryList = await Category.findOne({ id: rawExpense.categoryId }).lean()
+      rawExpense.icon = categoryList.icon
     rawExpense.date = rawExpense.date.toJSON().slice(0, 10)
     sum += rawExpense.amount
     expenses.push(rawExpense)
@@ -18,6 +18,27 @@ router.get('/', async(req, res) => {
     })
   )
   res.render('index', { expenses, sum })
+})
+
+//依選擇的類別顯示支出
+router.get('/category/:category', async (req, res) => {
+  const categoryArr = {home:'1',traffic:'2',entertainment:'3',food:'4',others:'5'}
+  const categoryEng = req.params.category
+  const categoryId = categoryArr[`${categoryEng}`]
+  const categoryList = await Category.findOne({ id:categoryId }).lean()
+  const categoryName = categoryList.name
+  let sum = 0
+  let expenses = []
+  let rawExpenses = await Expense.find({ categoryId }).lean()
+  rawExpenses.map(rawExpense => {
+    rawExpense.icon = categoryList.icon
+    rawExpense.date = rawExpense.date.toJSON().slice(0, 10)
+    sum += rawExpense.amount
+    expenses.push(rawExpense)
+  })
+  
+  res.render('index', { expenses, sum, categoryName })
+
 })
 
 module.exports = router
